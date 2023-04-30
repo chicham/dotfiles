88da60baaffef15e72783ee3696f560555d82cb1
Hicham Randrianarivo <hicham@randrianarivo.net>
2023-04-30 22:44:39 +0200

    add new splitjoin plugin

---
+++ dot_config/nvim/lua/custom/plugins/treej.lua
@@ -0,0 +1,12 @@
+return {
+  'Wansmer/treesj',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('treesj').setup({
+			use_default_keymaps = true,
+		})
+		vim.keymap.set('n', 'gT', require('treesj').toggle)
+		vim.keymap.set('n', 'gS', require('treesj').split)
+		vim.keymap.set('n', 'gJ', require('treesj').join)
+  end,
+}
