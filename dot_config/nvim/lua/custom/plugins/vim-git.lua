2e8a40badd76fdd0329ccc40c764f67433b2a812
Hicham Randrianarivo <hicham@randrianarivo.net>
2023-05-06 15:03:16 +0200

    add vim related plugins

---
+++ dot_config/nvim/lua/custom/plugins/vim-git.lua
@@ -0,0 +1,12 @@
+return {
+	'https://github.com/tpope/vim-git',
+	config = function ()
+		vim.keymap.set('n', '<leader>gbp', ':Pick<cr>', { silent = true })
+		vim.keymap.set('n', '<leader>gbs', ':Squash<cr>', { silent = true })
+		vim.keymap.set('n', '<leader>gbe', ':Edit<cr>', { silent = true })
+		vim.keymap.set('n', '<leader>gbr', ':Reword<cr>', { silent = true })
+		vim.keymap.set('n', '<leader>gbf', ':Fixup<cr>', { silent = true })
+		vim.keymap.set('n', '<leader>gbd', ':Drop<cr>', { silent = true })
+		vim.keymap.set('n', '<leader>gbb', ':Cycle<cr>', { silent = true })
+	end
+}
