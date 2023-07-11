9158aca4893c70f86e584756e189727e650d34c4
Hicham Randrianarivo <hicham@randrianarivo.net>
2023-07-11 16:04:20 +0200

    fix: whitespace

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 OP_VERSION=2.18.0
 BINDIR="${HOME}/.local/bin/"
 curl -l https://cache.agilebits.com/dist/1P/op2/pkg/v$OP_VERSION/op_linux_amd64_v$OP_VERSION.zip  > /tmp/op-$OP_VERSION.zip && unzip /tmp/op-$OP_VERSION.zip -d /tmp/op-$OP_VERSION && mv /tmp/op-$OP_VERSION/op ~/.local/bin && chmod u+x ~/.local/bin/op && rm -rf /tmp/op-$OP_VERSION*
- sh -c "$(curl -fsLS get.chezmoi.io)"
+sh -c "$(curl -fsLS get.chezmoi.io)"
