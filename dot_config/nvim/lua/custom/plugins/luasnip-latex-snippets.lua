4b93660187c3fb856511546f4034657f0e75a8f8
Hicham Randrianarivo <hicham@randrianarivo.net>
2023-07-11 14:40:22 +0200

    add: add new plugins

---
+++ dot_config/nvim/lua/custom/plugins/luasnip-latex-snippets.lua
@@ -0,0 +1,11 @@
+return {
+  "iurimateus/luasnip-latex-snippets.nvim",
+  -- replace "lervag/vimtex" with "nvim-treesitter/nvim-treesitter" if you're
+  -- using treesitter.
+  requires = { "L3MON4D3/LuaSnip", "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require'luasnip-latex-snippets'.setup({ use_treesitter = true })
+  end,
+  -- treesitter is required for markdown
+  ft = { "tex", "markdown" },
+}
---
+++ dot_config/nvim/lua/custom/plugins/vim-arsync.lua
@@ -0,0 +1,3 @@
+return {
+	"https://github.com/KenN7/vim-arsync"
+}
