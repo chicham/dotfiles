0229931fc51626889826b334d6c00c903fd57bfd
Hicham Randrianarivo <hicham@randrianarivo.net>
2023-07-11 15:38:39 +0200

    change: do not apply chezmoi before login with op

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@
 
 OP_VERSION=2.18.0
 curl -l https://cache.agilebits.com/dist/1P/op2/pkg/v$OP_VERSION/op_linux_amd64_v$OP_VERSION.zip  > /tmp/op-$OP_VERSION.zip && unzip /tmp/op-$OP_VERSION.zip -d /tmp/op-$OP_VERSION && mv /tmp/op-$OP_VERSION/op ~/.local/bin && chmod u+x ~/.local/bin/op && rm -rf /tmp/op-$OP_VERSION*
-eval $(op signin my)
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init
