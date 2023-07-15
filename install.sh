c1d026612399ec1c160b6a48711fb2235d839408
Hicham Randrianarivo <hicham@randrianarivo.net>
2023-07-16 01:02:10 +0200

    add: op install

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 # -u: exit on unset variables
 set -eu
 
+OP_VERSION=2.18.0
+curl -l https://cache.agilebits.com/dist/1P/op2/pkg/v$OP_VERSION/op_linux_amd64_v$OP_VERSION.zip  > /tmp/op-$OP_VERSION.zip && unzip /tmp/op-$OP_VERSION.zip -d /tmp/op-$OP_VERSION && mv /tmp/op-$OP_VERSION/op ~/.local/bin && chmod u+x ~/.local/bin/op && rm -rf /tmp/op-$OP_VERSION*
 if ! chezmoi="$(command -v chezmoi)"; then
   bin_dir="${HOME}/.local/bin"
   chezmoi="${bin_dir}/chezmoi"
