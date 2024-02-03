cb63e06690bde0773b664ab27aadfd98fa5e8c77
Hicham Randrianarivo <hicham+github@randrianarivo.net>
2024-02-03 22:24:27 +0100

    fix: tests with yes

--- install.sh
+++ install.sh
@@ -31,8 +31,8 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-set -- init --apply --source="${script_dir}"
+yes n | set -- init --apply --source="${script_dir}"
 
 echo "Running 'chezmoi $*'" >&2
 # exec: replace current process with chezmoi
-yes n | exec "$chezmoi" "$@"
+exec "$chezmoi" "$@"
