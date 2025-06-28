1dfcab2de2f66a0618ab46ad10df4dbead653a29
chicham <chicham@users.noreply.github.com>
2025-06-28 15:56:23 +0200

    refactor: Update Neovim plugin loading and event triggers
    This commit refactors the loading and event triggers for several Neovim plugins to improve startup performance and ensure proper functionality.
    
    Changes include:
    - : Changed  to  and .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to  and .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to .

--- dot_config/nvim/lua/custom/plugins/chezmoi.lua
+++ dot_config/nvim/lua/custom/plugins/chezmoi.lua
@@ -1,6 +1,6 @@
 return {
   'alker0/chezmoi.vim',
-  lazy = false,
+  event = { 'BufReadPre', 'BufNewFile' },
   init = function()
     -- This option is required.
     vim.g['chezmoi#use_tmp_buffer'] = true
--- dot_config/nvim/lua/custom/plugins/editorconfig.lua
+++ dot_config/nvim/lua/custom/plugins/editorconfig.lua
@@ -1,5 +1,6 @@
 return {
   'editorconfig/editorconfig-vim',
+  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
     vim.g.EditorConfig_exclude_patterns = { 'fugitive://.*', 'scp://.*' }
   end,
