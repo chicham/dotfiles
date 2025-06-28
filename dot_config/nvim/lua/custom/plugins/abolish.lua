da5a35709e2877ee2c56b2de697179dadb82ef75
chicham <chicham@users.noreply.github.com>
2025-06-28 15:56:23 +0200

    refactor: Update Neovim plugin loading and event triggers
    This commit refactors the loading and event triggers for several Neovim plugins to improve startup performance and ensure proper functionality.
    
    Changes include:
    - : Changed  to  and .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to  and .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to .
    - : Changed  to .

--- dot_config/nvim/lua/custom/plugins/abolish.lua
+++ dot_config/nvim/lua/custom/plugins/abolish.lua
@@ -1,3 +1,7 @@
 return {
   'tpope/vim-abolish',
+  cmd = { 'Abolish', 'Subvert', 'S' },
+  keys = {
+    { 'cr', mode = { 'n', 'v' }, desc = 'Coerce case' },
+  },
 }
--- dot_config/nvim/lua/custom/plugins/better-whitespace.lua
+++ dot_config/nvim/lua/custom/plugins/better-whitespace.lua
@@ -1,3 +1,9 @@
 return {
   'ntpeters/vim-better-whitespace',
+  event = { 'BufReadPost', 'BufNewFile' },
+  config = function()
+    vim.g.better_whitespace_enabled = 1
+    vim.g.strip_whitespace_on_save = 0
+    vim.g.strip_whitespace_confirm = 1
+  end,
 }
