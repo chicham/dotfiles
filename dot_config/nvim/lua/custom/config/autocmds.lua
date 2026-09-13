de0a2c3d1c67aa40f4f794c919cec8709a1b2afd
chicham <hicham.randrianarivo@artefact.com>
2026-09-13 16:51:52 +0200

    refactor(nvim): move keymaps and autocmds into lua/custom/config

---
+++ dot_config/nvim/lua/custom/config/autocmds.lua
@@ -0,0 +1,35 @@
+-- Autocommands that belong to the editor rather than to any plugin.
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- Remember cursor position when reopening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line
+    if line("'\"") > 0 and line("'\"") <= line("$") then
+      vim.cmd('normal! g`"')
+    end
+  end,
+})
+
+-- Auto-resize panes on terminal resize
+vim.api.nvim_create_autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
+
+-- Ensure wrap is always on for all buffers
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  pattern = "*",
+  callback = function()
+    vim.wo.wrap = true
+  end,
+})
