b29e70b9992c25150abffb0270705cccf7fb1d3b
chicham <hicham.randrianarivo@artefact.com>
2026-09-13 17:05:16 +0200

    refactor(nvim): move the conform spec into lua/custom/plugins

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -38,46 +38,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   -- Import plugins from custom directory
   { import = "custom.plugins" },
-  -- Code formatting
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
-        "<leader>cf",
-        function()
-          require("conform").format({ async = true, lsp_format = "fallback" })
-        end,
-        mode = "",
-        desc = "[C]ode [F]ormat",
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable autoformat for languages without a well standardized
-        -- coding style. Add filetypes here to opt them out.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_format = disable_filetypes[vim.bo[bufnr].filetype] and "never" or "fallback",
-        }
-      end,
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "ruff_format", "ruff_organize_imports" },
-        fish = { "fish_indent" },
-        sh = { "shfmt" },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-    },
-  },
-
   -- Autocompletion
   {
     "saghen/blink.cmp",
